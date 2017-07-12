b0a56cabffd6d48edd850cea3acd17701bd139f9
Alex Sartori <alex.sartori1997@gmail.com>
2017-07-12 16:14:34 +0200

    Add things TOO MANY IT'S A MESS

--- setup.sh
+++ setup.sh
@@ -4,10 +4,17 @@ cp bash/.bash{rc,_alises,_profile} ~/
 echo "[*] Copying ~/.vimrc..."
 cp conf/.vimrc ~/.vimrc
 
+echo "[*] Copying vim colorscheme..."
+cp conf/gruvbox.vim ~/.vim/colors/
+
 echo "[*] Copying ~/.gitconfig..."
 cp conf/.gitconfig ~/.gitconfig
+
+echo "[*] Copying i3 configuration..."
+cp -R conf/i3* ~/.config/
 
 echo "[*] Installing fonts to /usr/share/fonts/..."
 sudo cp -r fonts/* /usr/share/fonts/
 echo "    Refreshing font cache..."
 fc-cache -f -v > /dev/null
+
