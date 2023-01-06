3ab1f001d59ae18f419eadaa8aba359127cea969
Alex Sartori <alex.sartori1997@gmail.com>
2023-01-06 15:25:44 +0100

    Update config files

--- conf/fish/config.fish
+++ conf/fish/config.fish
@@ -16,7 +16,6 @@ set fish_greeting # Suppress default greeting
 fish_logo cyan blue white
 
 # Aliases
-alias atom="atom --portable"
 alias cat=lolcat
 alias dusg="du -sh * | sort -hr"
 # alias bat="/home/alex/Software/bat-v0.8.0-x86_64-unknown-linux-gnu/bat"
@@ -27,17 +26,21 @@ alias adb="/home/alex/Software/platform-tools/adb"
 alias iten="trans -s it -t en"
 alias enit="trans -s en -t it"
 
+# Hacky alias for atom because bugs
+function atom
+	/usr/bin/atom $argv --portable
+end
+
 # Paths
-set -x JAVA_HOME /usr/lib/jvm/java-17-openjdk
+set -x JAVA_HOME /usr/lib/jvm/java-18-openjdk
 set -x ANDROID_HOME $XDG_DATA_HOME/android
 set -x ATOM_HOME $XDG_DATA_HOME/atom
 set -x GEM_HOME $XDG_DATA_HOME/gem
-set ARM_GCC /home/alex/Mass\ Storage/ARM-GCC/bin
 set RBENV_HOME /home/alex/Software/rbenv
 set RBBUILD $RBENV_HOME/plugins/ruby-build/bin
 set -x RBENV_ROOT $XDG_DATA_HOME/rbenv
 
-set -x PATH $ARM_GCC $RBENV_HOME/bin $RBBUILD $PATH
+set -x PATH $RBENV_HOME/bin $RBBUILD $PATH
 
 # Configure better-ls
 set -x LS_COLORS (ls_colors_generator)
